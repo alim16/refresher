ebebbe86ebdac952da852540a157eb1eb8ec4273
mahamed <mohamed.tum@gmail.com>
2016-08-24 05:40:43 -0400

    fixed sub_seq.java

--- java_algos/sub_seq.java
+++ java_algos/sub_seq.java
@@ -7,7 +7,7 @@ public static void main(String[] args){
 	int[] arr2 = new int[] {3,1,4,1,5,9,2,6,5,3,5};
 	int[] arr3 = new int[] {2,7,1,8,2,8,1};
 	
-	length = subseqLength(arr);
+	length = subseqLength(arr3);
 	System.out.println("the length is: " + length);
 
 }
@@ -17,23 +17,23 @@ public static int subseqLength(int[] someArray){
 	int temp_seq = 0;
 	int longest = 0;
 	int prev = 0;
-	for(int i = 0; i < someArray.length-1; i++){	
+	for(int i : someArray){	
 		
-		if(prev < someArray[i]){
+		if(prev <= i){
 			//System.out.println(someArray[i]+" is more than: "+prev);
 			temp_seq = temp_seq + 1;
 		}else {
-			if(longest < temp_seq){
+			if( temp_seq > longest){
 			longest = temp_seq;
 			}
-			temp_seq = 0;
+			temp_seq = 1;
 			//System.out.println(someArray[i]+" is less than: "+prev);
 		}
-		prev = someArray[i];
+		prev = i;
 	}
 		
 
-	return longest + 1;
+	return longest;
 }
 
 
